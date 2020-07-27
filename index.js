86be9f193a01de3e2106f692a6418faead804630
Megan Comeau <mlcomeau99@gmail.com>
2020-07-27 21:05:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(activity= 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
+
+
+function wrapAdjective(wrapper = "*") {
+    return function(str) {
+      return `You are ${wrapper}${str}${wrapper}!`;
+    };
+  };
+
+const Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b
+};
+
+function actionApplyer(num, arrOfFuncs) {
+  let total = arrOfFuncs.reduce((sum, func) => func(num), 0);
+  return arrOfFuncs.length == 0 ? num : total - (arrOfFuncs.length - 1);
+}
